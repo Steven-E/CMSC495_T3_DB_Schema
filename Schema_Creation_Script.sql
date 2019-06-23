8de1a2787dec34862af45526725ac8111a0487d3
Steven Etienne <steven.a.etienne@gmail.com>
2019-06-23 13:50:04 -0500

    Initial Commit with draft of db schema.

---
+++ Schema_Creation_Script.sql
@@ -0,0 +1,70 @@
+CREATE DATABASE CMSC495_Team3;
+use CMSC495_Team3;
+
+CREATE TABLE IF NOT EXISTS userLogin(
+    UserName VARCHAR(100) NOT NULL,
+    Password VARCHAR(30)  NOT NULL,
+    UserEmail VARCHAR(255) NOT NULL,
+    UserId int PRIMARY KEY,
+    FirstName VARCHAR(100) NULL,
+    LastName VARCHAR(100) NULL,
+    Location VARCHAR(255) NULL    
+    -- AddDate DATETIME,
+    -- UpdatedTime DATETIME
+);
+
+CREATE TABLE IF NOT EXISTS socialMedia(
+    Vendor VARCHAR(25) PRIMARY KEY,
+    BaseUrl VARCHAR(100) NOT NULL,
+    AccountUrl VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS socialMediaAccount(
+    User_FK INT NOT NULL,
+    Vender_FK VARCHAR(25) NOT NULL,
+    AccountId VARCHAR(100) NOT NULL,
+    FOREIGN KEY (User_FK) REFERENCES userLogin(UserId),
+    FOREIGN KEY (Vender_FK) REFERENCES socialMedia(Vendor)
+);
+
+CREATE TABLE IF NOT EXISTS brewery(
+    BreweryName VARCHAR(255) NOT NULL,
+    Address VARCHAR(1024) NULL,
+    Phone VARCHAR(20) NULL,
+    BreweryId INT PRIMARY KEY,
+    UntappedId INT NULL,
+    BreweryDbId INT NULL,
+    LabelUrl VARCHAR(1024) NULL,
+    OrgType VARCHAR(50) NULL
+    -- AddDate DATETIME,
+    -- UpdatedTime DATETIME
+);
+
+CREATE TABLE IF NOT EXISTS Beer(
+    Brewery_FK INT NOT NULL,
+    BeerId INT PRIMARY KEY,
+    UntappdId INT NULL,
+    BreweryDbId INT NULL,
+    BeerName VARCHAR(255) NOT NULL,
+    ABV FLOAT NULL,
+    Description VARCHAR(2000) NULL,
+    IBU INT NULL,
+    Style VARCHAR(255) NULL,
+    ShortStyle VARCHAR(255) NULL,
+    LabelUrl VARCHAR(1024) NULL,
+    -- Type VARCHAR(2055) NULL,
+    FOREIGN KEY (Brewery_FK) REFERENCES brewery(BreweryId)
+    -- AddDate DATETIME NULL,
+    -- UpdateTime DATETIME NULL,
+);
+
+CREATE TABLE IF NOT EXISTS beerRankings(
+    User_FK INT NOT NULL,
+    Beer_FK INT NOT NULL,
+    Score FLOAT NULL,
+    UserRankPosition INT NULL,
+    HaveTried BOOLEAN NOT NULL,
+    
+    FOREIGN KEY (User_FK) REFERENCES userLogin(UserId),
+    FOREIGN KEY (Beer_FK) REFERENCES beer(BeerId)
+);
