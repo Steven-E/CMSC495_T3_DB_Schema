3bf8dd797ca5e3dd4cdbaeaaa4d36ae44d74ae74
Steven Etienne <steven.a.etienne@gmail.com>
2019-06-26 21:01:30 -0500

    Schema updates

--- Schema_Creation_Script.sql
+++ Schema_Creation_Script.sql
@@ -1,35 +1,36 @@
 CREATE DATABASE CMSC495_Team3;
 use CMSC495_Team3;
 
-CREATE TABLE IF NOT EXISTS userLogin(
+CREATE TABLE IF NOT EXISTS UserInfo(
     UserName VARCHAR(100) NOT NULL,
     Password VARCHAR(50)  NOT NULL,
     UserEmail VARCHAR(255) NOT NULL,
-    UserId int PRIMARY KEY,
+    UserId int PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    UntappdId int NULL,
     FirstName VARCHAR(100) NULL,
     LastName VARCHAR(100) NULL,
     Location VARCHAR(255) NULL    
 );
 
-CREATE TABLE IF NOT EXISTS socialMedia(
+CREATE TABLE IF NOT EXISTS SocialMedia(
     Vendor VARCHAR(25) PRIMARY KEY,
     BaseUrl VARCHAR(100) NOT NULL,
     AccountUrl VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS socialMediaAccount(
+CREATE TABLE IF NOT EXISTS SocialMediaAccount(
     User_FK INT NOT NULL,
     Vender_FK VARCHAR(25) NOT NULL,
     AccountId VARCHAR(100) NOT NULL,
-    FOREIGN KEY (User_FK) REFERENCES userLogin(UserId),
-    FOREIGN KEY (Vender_FK) REFERENCES socialMedia(Vendor)
+    FOREIGN KEY (User_FK) REFERENCES UserInfo(UserId),
+    FOREIGN KEY (Vender_FK) REFERENCES SocialMedia(Vendor)
 );
 
-CREATE TABLE IF NOT EXISTS brewery(
+CREATE TABLE IF NOT EXISTS Brewery(
     BreweryName VARCHAR(255) NOT NULL,
     Address VARCHAR(1024) NULL,
     Phone VARCHAR(20) NULL,
-    BreweryId INT PRIMARY KEY,
+    BreweryId INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
     UntappedId INT NULL,
     BreweryDbId INT NULL,
     LabelUrl VARCHAR(1024) NULL,
@@ -38,29 +39,26 @@ CREATE TABLE IF NOT EXISTS brewery(
 
 CREATE TABLE IF NOT EXISTS Beer(
     Brewery_FK INT NOT NULL,
-    BeerId INT PRIMARY KEY,
+    BeerId INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
     UntappdId INT NULL,
     BreweryDbId INT NULL,
     BeerName VARCHAR(255) NOT NULL,
     ABV FLOAT NULL,
-    Description VARCHAR(2000) NULL,
+    Description VARCHAR(2048) NULL,
     IBU INT NULL,
     Style VARCHAR(255) NULL,
     ShortStyle VARCHAR(255) NULL,
     LabelUrl VARCHAR(1024) NULL,
-    FOREIGN KEY (Brewery_FK) REFERENCES brewery(BreweryId)
+    FOREIGN KEY (Brewery_FK) REFERENCES Brewery(BreweryId)
 );
 
-CREATE TABLE IF NOT EXISTS beerRankings(
+CREATE TABLE IF NOT EXISTS UserBeerRanking(
     User_FK INT NOT NULL,
     Beer_FK INT NOT NULL,
     Score FLOAT NULL,
     UserRankPosition INT NULL,
     HaveTried BOOLEAN NOT NULL,
-    UntappdReviewId INT NULL,
-    BreweryDbId INT NULL,
-    Description VARCHAR(2048) NULL,
     UserReview VARCHAR (2048) NULL,    
-    FOREIGN KEY (User_FK) REFERENCES userLogin(UserId),
-    FOREIGN KEY (Beer_FK) REFERENCES beer(BeerId)
+    FOREIGN KEY (User_FK) REFERENCES UserInfo(UserId),
+    FOREIGN KEY (Beer_FK) REFERENCES Beer(BeerId)
 );
